fdc6907e5f60f487ae9b63c60d5676294baf0313
Rhys Hansen <rhyshonline@gmail.com>
2017-09-10 14:57:32 -0500

    Minor formatting improvements

--- imagecolor/__init__.py
+++ imagecolor/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#coding=UTF-8
+# coding=UTF-8
 from .average import average
 from .average import average_images
 from .average import directory_average
@@ -11,8 +11,8 @@ from .loadsave import results_save_csv
 from .loadsave import results_load_csv
 
 major_version = 1
-minor_version = 1
-build_version = 8
+minor_version = 2
+build_version = 0
 
 __author__ = 'Rhys Hansen'
 __copyright__ = "Copyright 2017, Rhys Hansen"
--- imagecolor/average.py
+++ imagecolor/average.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-#coding=UTF-8
+# coding=UTF-8
+
 import imghdr
 import logging
 import os
@@ -29,7 +30,9 @@ SOFTWARE."""
 
 logger = logging.getLogger(__name__)
 
-def average(image, name=None, downsample=True, max_size=100, alpha_threshold=None):
+
+def average(image, name=None, downsample=True,
+            max_size=100, alpha_threshold=None):
     """Averages a single image from a file or file-like object.
     Arguments
     image: str
@@ -52,16 +55,15 @@ def average(image, name=None, downsample=True, max_size=100, alpha_threshold=Non
     logger.debug('Image name: %s', name)
     try:
         im = Image.open(image)
-        logger.debug('Image opened. Dementions %d x %d', im.size[0], im.size[1])
-        if (im.size[0] > max_size or im.size[1] > max_size) and downsample == True:
-            im.thumbnail((max_size,max_size))
+        logger.debug('Image opened. Dimensions %d x %d',
+                     im.size[0], im.size[1])
+        if (im.size[0] > max_size or im.size[1] > max_size)
+        and downsample is True:
+            im.thumbnail((max_size, max_size))
             logger.debug('Image resized to %d x %d', im.size[0], im.size[1])
         grid = im.load()
         pixels = []
-        pixelcount = 0
-        r_total = 0
-        g_total = 0
-        b_total = 0
+        pixelcount, r_total, g_total, b_total = 0, 0, 0, 0
         for x in range(im.size[0]):
             for y in range(im.size[1]):
                 currentpx = grid[x, y]
@@ -83,13 +85,15 @@ def average(image, name=None, downsample=True, max_size=100, alpha_threshold=Non
         r_avg = int(r_total / pixelcount)
         g_avg = int(g_total / pixelcount)
         b_avg = int(b_total / pixelcount)
-        logger.debug('average result: Name=%s, R=%d, G=%d, B=%d', name, r_avg, g_avg, b_avg)
-        return({'name':name, 'red':r_avg, 'green':g_avg, 'blue':b_avg})
+        logger.debug('average result: Name=%s, R=%d, G=%d, B=%d',
+                     name, r_avg, g_avg, b_avg)
+        return({'name': name, 'red': r_avg, 'green': g_avg, 'blue': b_avg})
     except Exception as e:
         logger.warning('Exception %s', e)
         logger.debug('average Traceback', exc_info=True)
     else:
         return(None)
+
 
 def average_images(dir_in):
     """Accepts the path to a directory averages each individual
@@ -110,14 +114,15 @@ def average_images(dir_in):
     images = []
     results = []
     files = [f for f in os.listdir(dir_in)
-        if os.path.isfile(os.path.join(dir_in, f))]
+             if os.path.isfile(os.path.join(dir_in, f))]
     for f in files:
         filepath = os.path.join(dir_in, f)
-        if imghdr.what(filepath) in ['jpeg','png']:
+        if imghdr.what(filepath) in ['jpeg', 'png']:
             images.append(filepath)
     with Pool(cpus) as p:
         results = (p.map(average, images))
     return(results)
+
 
 def directory_average(dir_in, name=None):
     """Averages the images in the directory into a directory average.
@@ -135,17 +140,14 @@ def directory_average(dir_in, name=None):
         cpus = 4
         logger.warning('Number of CPUs not found. Setting default to %s', cpus)
     filepaths = []
-    r_total = 0
-    b_total = 0
-    g_total = 0
-    imagecount = 0
+    imagecount, r_total, g_total, b_total = 0, 0, 0, 0
     if name is None:
         dir_name = os.path.normpath(dir_in)
         dir_name = dir_name.split(os.sep)
     for filename in os.listdir(dir_in):
-        filepath = os.path.join(dir_in,filename)
+        filepath = os.path.join(dir_in, filename)
         try:
-            if imghdr.what(filepath) in ['jpeg','png']:
+            if imghdr.what(filepath) in ['jpeg', 'png']:
                 filepaths.append(filepath)
         except(IsADirectoryError):
             logger.debug('Directory %s found, Skipping', filename)
@@ -154,9 +156,9 @@ def directory_average(dir_in, name=None):
             results = (p.map(average, filepaths))
     for result in results:
         try:
-            r_total += result.get('red')
-            b_total += result.get('green')
-            g_total += result.get('blue')
+            r_total += result['red']
+            b_total += result['green']
+            g_total += result['blue']
             imagecount += 1
         except TypeError:
             logger.debug('Result not vaild. Skipping', exc_info=True)
@@ -168,10 +170,13 @@ def directory_average(dir_in, name=None):
         r_avg = int(r_total / imagecount)
         g_avg = int(g_total / imagecount)
         b_avg = int(b_total / imagecount)
-        return({'name':dir_name[-1], 'red':r_avg, 'green':g_avg, 'blue':b_avg})
+        return({'name': dir_name[-1], 'red': r_avg,
+                'green': g_avg, 'blue': b_avg})
     else:
-        logger.warning("No images in %s directory successfully averaged. Returning None", dir_name[-1])
+        logger.warning("No images in %s directory successfully averaged. "
+                       "Returning None", dir_name[-1])
         return(None)
+
 
 def nested_directory_average(root_dir):
     """Accepts the path to a directory and walks all the enclosed
@@ -183,25 +188,25 @@ def nested_directory_average(root_dir):
     return [{'name':directory, 'red':r_avg, 'green':g_avg, 'blue':b_avg},
             {'name':directory, 'red':r_avg, 'green':g_avg, 'blue':b_avg}]
     """
-    filtered_dirs= []
+    filtered_dirs = []
     results = []
     sub_dirs = [d[0] for d in os.walk(root_dir)]
     for current_dir in sub_dirs:
         for current_file in os.listdir(current_dir):
             filepath = os.path.join(current_dir, current_file)
             try:
-                if imghdr.what(filepath) in ['jpeg','png']:
+                if imghdr.what(filepath) in ['jpeg', 'png']:
                     filtered_dirs.append(current_dir)
                     logger.debug('Image found in directory %s. '
-                        'Appending to filtered directories',
-                        current_dir.split(os.sep)[-1])
+                                 'Appending to filtered directories',
+                                 current_dir.split(os.sep)[-1])
                     break
             except(IsADirectoryError):
                 pass
     for dir_path in filtered_dirs:
         result = directory_average(dir_path)
         try:
-            if result != None:
+            if result is not None:
                 results.append(result)
         except TypeError:
             pass
