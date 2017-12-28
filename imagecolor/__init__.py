f92ff0f1db5f98fda59ce91b8644ed13b7c63f2e
Rhys Hansen <rhyshonline@gmail.com>
2017-12-27 19:45:22 -0600

    Update imports with refactored functions

--- imagecolor/__init__.py
+++ imagecolor/__init__.py
@@ -1,21 +1,20 @@
 """imagecolor initialization."""
 
-__all__ = ["core_average", "file_average",
-           "average_images", "directory_average",
-           "nested_directory_average", "results_line",
-           "results_rectangle", "results_save_csv",
+__all__ = ["core_average",
+           "file_average",
+           "directory_average",
+           "single_directory_average",
+           "nested_directory_average",
+           "results_line",
+           "results_rectangle",
+           "results_save_csv",
            "results_load_csv"]
 
-from .average import core_average
-from .average import file_average
-from .average import average_images
-from .average import directory_average
-from .average import nested_directory_average
+from .average import core_average, file_average, directory_average
+from .average import single_directory_average, nested_directory_average
 
-from .loadsave import results_line
-from .loadsave import results_rectangle
-from .loadsave import results_save_csv
-from .loadsave import results_load_csv
+from .loadsave import results_line, results_rectangle
+from .loadsave import results_save_csv, results_load_csv
 
 
 __author__ = 'Rhys Hansen'
